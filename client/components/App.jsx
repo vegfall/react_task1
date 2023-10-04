97bf3a1802514e79e9ffce74b8ca2399b65e450b
Vegard Falldalen <vegfall@gmail.com>
2023-10-04 14:49:54 +0200

    Added header, maincontent, app and footer component

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainContent from "./MainContent";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+};
