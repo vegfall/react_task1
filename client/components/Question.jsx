00fa2cc76d15fbe30269f3d3941a9968926141f0
Vegard Falldalen <vegfall@gmail.com>
2023-10-11 20:03:41 +0200

    Question buttons added

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
 export default function Question(props) {
-    let answers = "";
-
-    console.log(props.answers)
-
-    for (const [key, value] in Object.entries(props.answers)) {
-        answers += " " + `${value}`;
-    }
-
+    const answers = Object.entries(props.answers)
+        .filter(value => value[1])
+        .map((question) => {
+            return <button key={question[0]}>{question[1]}</button>
+        })
 
     return (
         <>
