5cad9d2f6d53163058e5110c1b39032aee4c168b
Vegard Falldalen <vegfall@gmail.com>
2023-10-14 09:16:14 +0200

    Started v2, moved components to new folder

--- client/components/Response.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Response(props) {
-
-
-    return (
-        <button key={props[0]} className="questionButton">{props[1]}</button>
-    );
-};
--- client/old/Question.jsx
+++ client/old/Question.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import Response from "./Response";
+import AnswerButton from "./AnswerButton";
 
 export default function Question(props) {
+    const testing = Object.entries(props)
+
+    console.log(testing)
+
     const answer = Object.entries(props.answers)
         .filter(question => question[1])
         .map((question) => {
-            return <Response {...question}/>
+            return <AnswerButton {...question}/>
         });
 
     return (
